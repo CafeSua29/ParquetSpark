5e2affee0bdb1541db8675f066a19733f1979a18
CafeSua29 <luongbaphananh@gmail.com>
2024-09-16 15:02:21 +0700

    7

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,6 +2,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions.Window
+import spark.implicits._
 
 object Main extends App {
     val spark = SparkSession.builder()
